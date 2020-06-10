e82222aa0355f19de7281ff9dee052449ea9d39e
RishanthRajendhran <rishanthrajendhran@outlook.com>
2020-06-10 13:42:44 +0530

    Social Share

--- components/DishDetailComponent.js
+++ components/DishDetailComponent.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Alert, PanResponder } from 'react-native';
+import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Alert, PanResponder, Share } from 'react-native';
 import {Button,Rating,Input} from "react-native-elements";
 import { Card,Icon } from "react-native-elements";
 import { withNavigation } from "react-navigation";
@@ -76,6 +76,16 @@ function RenderDish(props) {
     })
     ).current;
 
+    const shareDish = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: title + ': ' + message + ' ' + url,
+            url: url
+        },{
+            dialogTitle: 'Share ' + title
+        })
+    }
+
     if(dish!=null) {
         return(
             <Animatable.View animation="fadeInDown" duration={2000} delay={1000}
@@ -107,6 +117,15 @@ function RenderDish(props) {
                             color='#512DA8'
                             onPress={() => props.addComm()}
                         />
+                        <Icon
+                            raised
+                            reverse
+                            name='share'
+                            type='font-awesome'
+                            color='#51D2A8'
+                            style={styles.cardItem}
+                            onPress={() => shareDish(dish.name, dish.description, baseUrl + dish.image)} 
+                            />
                     </View>
                 </Card>
             </Animatable.View>
